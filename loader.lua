4c88f34b6ac1c50abb86e98980409b3253154ae5
el-the-mundo <130784209+el-the-mundo@users.noreply.github.com>
2025-02-02 13:23:39 +0000

    Creation of project

--- loader.lua
+++ loader.lua
@@ -1,1 +1,12 @@
+local Library = loadstring(game:HttpGet("https://github.com/kgukmz/Larceny/raw/refs/heads/main/Modules/ImGui.lua"))()
+
+local Tabs = (
+    
+)
+
+local Window = Library:CreateWindow({
+	Title = "Larceny [Beta]",
+	Size = UDim2.new(0, 350, 0, 370),
+	Position = UDim2.new(0.5, 0, 0, 70)
+})
 
