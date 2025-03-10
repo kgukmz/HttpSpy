9593d067500ebde017370338b82cfdf86b525a21
kgukmz <144381271+kgukmz@users.noreply.github.com>
2025-03-10 19:33:16 +0000

    Update and rename loader.lua to Main.lua

--- loader.lua
+++
@@ -1,7 +0,0 @@
-local Library = loadstring(game:HttpGet("https://github.com/kgukmz/Larceny/raw/refs/heads/main/Modules/ImGui.lua"))()
-
-local Window = Library:CreateWindow({
-	Title = "Larceny [Beta]",
-	Size = UDim2.new(0, 350, 0, 430),
-	Position = UDim2.new(0.5, 0, 0, 70),
-})
